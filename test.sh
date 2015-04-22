7891de82ebf9f78920bee5db2a98a7e77981409e
Jan Ťulák <github@tulak.me>
2015-04-22 19:00:52 +0200

    Using mktemp instead of /tmp/something
    Signed-off-by: Jan Ťulák <github@tulak.me>

--- test.sh
+++ test.sh
@@ -19,22 +19,19 @@
 ERRS=0
 TESTS=0
 
-rm -f /tmp/*-correct
-rm -f /tmp/*-cracked
-rm -f /tmp/*-cipher
+CIPHER=$(mktemp -t "$0")
+CORRECT=$(mktemp -t "$0")
+CRACKED=$(mktemp -t "$0")
 
 function check()
 {
 	TESTS=$((TESTS + 1))
-	diff /tmp/correct /tmp/cracked >/dev/null
+	diff "$CORRECT" "$CRACKED" >/dev/null
 	if [ $? -eq 0 ] ; then
 		echo -n "."
 	else
 		echo -n "!" 
 		ERRS=$((ERRS + 1))
-		mv /tmp/cipher /tmp/$TESTS-cipher 2>/dev/null
-		mv /tmp/correct /tmp/$TESTS-correct 2>/dev/null
-		mv /tmp/cracked /tmp/$TESTS-cracked 2>/dev/null
 	fi
 }
 
@@ -43,36 +40,36 @@ function crack()
 	key="$1"
 	cipher="$2"
 	if [ "$key" = "" ]; then
-		./test_with_key.py $cipher | tail -n 1 > /tmp/cracked
+		./test_with_key.py $cipher | tail -n 1 > "$CRACKED"
 	else
-		./test_with_key.py "$key" $cipher | tail -n 1 > /tmp/cracked
+		./test_with_key.py "$key" $cipher | tail -n 1 > "$CRACKED"
 	fi
 }
 
 # Some provided cryptos
 
 crack "" 19e27c9b5411608f9a8909c21eee2a61198b124de41f6e8d41ff1f22b3c60e789cd370bf2e2c88ad08643a5769e99264a5659ee34fe4dc03c269bd2af255edcf
-echo "The magic words are squeamish ossifrage">/tmp/correct
+echo "The magic words are squeamish ossifrage">"$CORRECT"
 check
 
 crack "" ac1939ede54a02b3ce6504945063ec7a0be0da33b72b68783019102ebe6b32d3
-echo -e "123456789ABCDE\x02">/tmp/correct
+echo -e "123456789ABCDE\x02">"$CORRECT"
 check
 
 crack "" 92fd508cadb2bef14554d43a79394332ff00adebd197025f980f829c95f57305
-echo "-- Snip --">/tmp/correct
+echo "-- Snip --">"$CORRECT"
 check
 
 crack "" fa485ab028cb239a39a9e52df1ebf4c30911b25d73f8906cc45b6bf87f7a693f47609094ccca42050ad609bb3cf979ac
-echo "Ahoj, jak se Ti vede0000000000?">/tmp/correct
+echo "Ahoj, jak se Ti vede0000000000?">"$CORRECT"
 check
 
  crack "sixteen_byte_key" a1372965b3a651999c1ef019fef1402c32f7847fdd0f2b29a0d3135dc4dd54c9507c91f2f1a7fc863a572db4bab0d64e13d5469a2e31f2d8c2ada6f4cca72f4623cd39bc7f4cb16df995392c98b785512058c27fb543ac15d6aad12942f87d336e27216fd0ab7839fe64ead0b6e20fbf23e5b5ca8c4fd074772bb3eb9a8efbd65be42c0ead9a4a20ff057b0e615d932cc80fe7e886e96cd46adef0edaf63422153f973399a70751c89e766ffa360be7cc3d707018195cf19244bae6258abcc6a
- echo "There are two types of cyptography: one that allows the Government to use brute force to break the code, and one that requires the Government to use brute force to break you.">/tmp/correct
+ echo "There are two types of cyptography: one that allows the Government to use brute force to break the code, and one that requires the Government to use brute force to break you.">"$CORRECT"
  check
  
  crack "sixteen_byte_key" 5a36670ff5cb94ba2509e3f0ae74db879e239644b5e7a924f4c9fff6368500c3d84e9aa2f84a80d33ee7f2a18d4f4c5f65214fcf4f42dba2f5da24d7eb19e0b1cfca111cc6e4f7ef88e745f91c4632a54764090361083afc90deefef42737984a3461033d448fa012a979a2984b4c43640fb1e77bbf23890e45c0a69c92273c8dd23e58ceb1015c51a90b40a594aedc553e54b9452b735c8c8b677a37ac2d3962c75c1c2adb3b45698583da8c5a58d22dde58bb5ed7f66bb6874d305822ac0e6
- echo "Key escrow to rule them all; key escrow to find them. Key escrow to bring them all and in the darkness bind them. In the land of surveillance where Big Brother lies.">/tmp/correct
+ echo "Key escrow to rule them all; key escrow to find them. Key escrow to bring them all and in the darkness bind them. In the land of surveillance where Big Brother lies.">"$CORRECT"
  check
 
 # custom crypto
@@ -80,9 +77,9 @@ for key in "0000000000000000" "abcdefghijklmnopqrstuvwx"
 do
 	for text in "holaholaholaholahola hola hola hola" "zkouska zkouska" "abc" "Ahoj, jak se Ti vede0000000000?"
 	do
-		echo "$text" > /tmp/correct
+		echo "$text" > "$CORRECT"
 		cipher=$(./encrypt.py "$text" "$key")
-		echo "$cipher" > /tmp/cipher
+		echo "$cipher" > "$CIPHER"
 		crack "$key" "$cipher"
 		check
 	done
@@ -92,3 +89,4 @@ done
 echo
 echo "Tests run: $TESTS, tests failed: $ERRS"
 
+rm -f "$CIPHER" "$CORRECT" "$CRACKED"
